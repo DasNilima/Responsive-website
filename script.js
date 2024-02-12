23d82fc29bebd7ec67998e134c2fdc996410bf71
DasNilima <nilima_sust@yahoo.com>
2024-02-12 12:04:08 -0800

    add hamburger meu feature

--- script.js
+++ script.js
@@ -220,4 +220,12 @@ function nextPage() {
 }
 
 
+// <!-- Hamburger Menu -->
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
 
+btn.addEventListener('click', () => {
+  btn.classList.toggle('open')
+  nav.classList.toggle('flex')
+  nav.classList.toggle('hidden')
+})
