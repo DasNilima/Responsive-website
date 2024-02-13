8f1faca61543718e00f0449c0553433d11867ac9
DasNilima <nilima_sust@yahoo.com>
2024-02-12 21:13:02 -0800

    add search functionality

--- script.js
+++ script.js
@@ -1,156 +1,3 @@
-// let userData = []
-// const tableBody = document.querySelector('#table_body');
-// const scoreHeader = document.querySelector('#score_column');
-// let scoreAscending = false;
-// const getUserRankingsAsync = async (url) => {
-//   // this is aync await syntax for fetching data.
-//   const response = await fetch(
-//     url
-//   );
-//   const userData = await response.json();
-//   return userData;
-// }
-
-// // it will be passed inside filter function which is a higher order function 
-// // and its an callback function.
-// const filterValues = item => item.score > 30;
-// const sortUserScores = () => {
-//   const tempData = [...userData];
-//   tempData.sort((a, b) => scoreAscending ? a.score - b.score : b.score - a.score);
-//   const filteredData = tempData.filter(filterValues);
-//   const newData = tempData.map(item => ({name: item.name, score: item.score}))
-//   scoreAscending=!scoreAscending;
-//   while (tableBody.firstChild) {
-//     tableBody.removeChild(tableBody.firstChild);
-//   }
-//   console.log(tempData, newData);
-//   renderTableRows(filteredData);
-// }
-
-// // Adding event handlers to an element
-// scoreHeader.addEventListener('click', sortUserScores)
-// const page = 4;
-// let currPage = 1;
-// const renderTableRows = (data) => {
-//   data.filter((row, i) => {
-//     let start = (currPage-1)*page;
-//     let end =currPage*page;
-//     if(i >= start && i < end) return true;
-//   }).forEach((user, i) => {
-//     const tRow = document.createElement('tr');
-//     const td1 = document.createElement('td');
-//     const td2 = document.createElement('td');
-//     const td3 = document.createElement('td');
-//     const td4 = document.createElement('td');
-//     td1.textContent = i + 1;
-//     td1.classList.add('p-3',`${i%2===0 ? 'bg-veryPaleRed' : 'bg-veryLightGray'}`)
-//     td2.textContent = user.name;
-//     td2.classList.add('p-3', `${i % 2 === 0 ? 'bg-veryPaleRed' : 'bg-veryLightGray'}`);
-//     td3.textContent = user.country;
-//     td3.classList.add('p-3', `${i % 2 === 0 ? 'bg-veryPaleRed' : 'bg-veryLightGray'}`);
-//     td4.textContent = user.score;
-//     td4.classList.add('p-3', `${i % 2 === 0 ? 'bg-veryPaleRed' : 'bg-veryLightGray'}`);
-//     tRow.appendChild(td1);
-//     tRow.appendChild(td2);
-//     tRow.appendChild(td3);
-//     tRow.appendChild(td4);
-//     tableBody.appendChild(tRow)
-//   })
-// }
-// const main = async () => {
-//   userData = await getUserRankingsAsync(
-//     'https://64743e827de100807b1a84ab.mockapi.io/api/v1/leaderboard/users'
-//   );
-//   // fetchData(
-//   //   'https://64743e827de100807b1a84ab.mockapi.io/api/v1/leaderboard/users'
-//   // ).then(data => renderTableRows(data));
-//   renderTableRows(userData)
-  
-// };
-// main();
-
-// let userData = []
-// const tableBody = document.querySelector('#table_body');
-// const scoreHeader = document.querySelector('#score_column');
-// let scoreAscending = false;
-
-
-// const getUserRankingsAsync = async (url) => {
-//   // this is aync await syntax for fetching data.
-//   const response = await fetch(
-//     url
-//   );
-//   const userData = await response.json();
-
-//   return userData;
-// }
-
-// // const fetchData = (url) => {
-// //   return fetch(url).then(res => res.json()).then(res => res)
-// // }
-
-
-// // it will be passed inside filter function which is a higher order function 
-// // and its an callback function.
-// const filterValues = item => item.score > 30;
-
-
-// const sortUserScores = () => {
-//   const tempData = [...userData];
-//   tempData.sort((a, b) => scoreAscending ? a.score - b.score : b.score - a.score);
-//   const filteredData = tempData.filter(filterValues);
-//   const newData = tempData.map(item => ({name: item.name, score: item.score}))
-//   scoreAscending=!scoreAscending;
-//   while (tableBody.firstChild) {
-//     tableBody.removeChild(tableBody.firstChild);
-//   }
-//   console.log(tempData, newData);
-//   renderTableRows(filteredData);
-// }
-
-// // Adding event handlers to an element
-// // scoreHeader.addEventListener('click', sortUserScores)
-
-
-
-
-
-// const renderTableRows = (data) => {
-//   data?.forEach((user, i) => {
-//     const tRow = document.createElement('tr');
-//     const td1 = document.createElement('td');
-//     const td2 = document.createElement('td');
-//     const td3 = document.createElement('td');
-//     const td4 = document.createElement('td');
-//     td1.textContent = i + 1;
-//     td1.classList.add('p-3',`${i%2===0 ? 'bg-white' : 'bg-sky-50'}`)
-//     td2.textContent = user.name;
-//     td2.classList.add('p-3', `${i % 2 === 0 ? 'bg-white' : 'bg-sky-50'}`);
-//     td3.textContent = user.country;
-//     td3.classList.add('p-3', `${i % 2 === 0 ? 'bg-white' : 'bg-sky-50'}`);
-//     td4.textContent = user.score;
-//     td4.classList.add('p-3', `${i % 2 === 0 ? 'bg-white' : 'bg-sky-50'}`);
-//     tRow.appendChild(td1);
-//     tRow.appendChild(td2);
-//     tRow.appendChild(td3);
-//     tRow.appendChild(td4);
-//     tableBody.appendChild(tRow)
-//   })
-// }
-
-// const main = async () => {
-//   userData = await getUserRankingsAsync(
-//     'https://64743e827de100807b1a84ab.mockapi.io/api/v1/leaderboard/users'
-//   );
-//   // fetchData(
-//   //   'https://64743e827de100807b1a84ab.mockapi.io/api/v1/leaderboard/users'
-//   // ).then(data => renderTableRows(data));
-//   renderTableRows(userData)
-  
-// };
-// main();
-
-
 document.addEventListener('DOMContentLoaded', init, false);
 let data, table, sortCol;
 let sortAsc = false;
@@ -161,7 +8,7 @@ async function init() {
   
   // Select the table (well, tbody)
   table = document.querySelector('#table tbody');
-  // get the cats
+  // get the users
   let resp = await fetch('https://64743e827de100807b1a84ab.mockapi.io/api/v1/leaderboard/users');
   data = await resp.json();
   renderTable();
@@ -182,7 +29,7 @@ function renderTable() {
         let start = (curPage-1)*pageSize;
         let end =curPage*pageSize;
         if(i >= start && i < end) return true;
-  }).forEach((user,i) => {
+  }).forEach((user, i) => {
       result += `<tr>
     <td>${i+1}</td>
       <td>${user.name}</td>
@@ -193,6 +40,18 @@ function renderTable() {
   });
   table.innerHTML = result;
 }
+
+function previousPage() {
+  if(curPage > 1) curPage--;
+  renderTable();
+}
+
+function nextPage() {
+  if((curPage * pageSize) < data.length) curPage++;
+  renderTable();
+}
+
+//  sort functionality
 
 function sort(e) {
   let thisSort = e.target.dataset.sort;
@@ -207,17 +66,31 @@ function sort(e) {
   renderTable();
 }
 
+//  search Functionality
 
+function search() {
+  let input, filter, table, tr, td, i, txtValue
+  input = document.getElementById('search')
+  filter = input.value.toUpperCase()
+  table = document.getElementById('table')
+  tr = table.getElementsByTagName('tr')
 
-function previousPage() {
-  if(curPage > 1) curPage--;
-  renderTable();
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName('td')[1]
+    if (td) {
+      txtValue = td.textContent || td.innerText
+
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = ""
+      
+      }
+      else {
+        tr[i].style.display = "none"
+      }
+    }
+  }
 }
 
-function nextPage() {
-  if((curPage * pageSize) < data.length) curPage++;
-  renderTable();
-}
 
 
 // <!-- Hamburger Menu -->
